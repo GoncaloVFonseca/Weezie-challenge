407932a91d30864862f6c61f406c4839f7688c6d
Goncalo Fonseca <gvfonseca@gmail.com>
2023-03-27 21:22:35 +0100

    added map

---
+++ src/components/Map.vue
@@ -0,0 +1,30 @@
+<script>
+import "vue3-openlayers/dist/vue3-openlayers.css";
+</script>
+
+<template>
+  <ol-map
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+   class="map"
+  >
+    <ol-view
+      ref="view"
+      :center="center"
+      :rotation="rotation"
+      :zoom="zoom"
+      :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+  </ol-map>
+</template>
+
+<style>
+.map {
+    height: 500px;
+    width: 1200px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import OpenLayersMap from 'vue3-openlayers'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(OpenLayersMap)
+
+app.mount('#app')
